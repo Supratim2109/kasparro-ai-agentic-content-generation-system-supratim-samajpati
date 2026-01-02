d265611302f8c68d6b78dc70056faee400fb4b89
Supratim <124481297+Supratim2109@users.noreply.github.com>
2026-01-02 17:54:08 +0530

    Delete core directory

--- core/task.py
+++
@@ -1,7 +0,0 @@
-class Task:
-    def __init__(self, task_type: str,payload:str):
-        self.type=task_type
-        self.payload=payload
-
-    def __repr__(self):
-        return f"Task(type={self.type})"
